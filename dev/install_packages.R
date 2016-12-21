c76443558b4ac8b89c4e09d537bd4d594c5ba6b8
tzerk <XnIASwkTCBlGgudcCsml>
2016-12-21 11:41:25 +0100

    make dev image dynamic
    The R script is copied into the container and executed. The script itself makes use of the GitHub API checking for existing repo branches and installing the "shortest" of the 'dev_' branches, if available.

---
+++ dev/install_packages.R
@@ -0,0 +1,27 @@
+# List of packages to be installed
+packages <- c("Luminescence", "RLumModel", "RLumShiny")
+
+for (package in packages) {
+
+  # fetch available repo branches using the GitHub API
+  # https://developer.github.com/
+  # We explicitly assume that development branch names start with "dev_"
+  branches <- grep(pattern = "^dev_",
+                   jsonlite::fromJSON(paste0("https://api.github.com/repos/R-Lum/", package,"/branches"))$name,
+                   value = TRUE)
+
+  # We further assume that amongst all branches that start with "dev_" the
+  # one with the shortest name is the actual main development branch.
+  # Fallback: revert to master branch if no dev_ branch exists
+  if (length(branches) >= 1)
+    dev_branch <- branches[which.min(nchar(branches))]
+  else
+    dev_branch <- "master"
+
+  # install package
+  devtools::install_github(paste0("r-lum/", package, "@", dev_branch))
+}
+
+# clean up
+closeAllConnections()
+gc()
