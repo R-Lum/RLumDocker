38e7545b0e1a1892460eeb17189a04b831bf2a43
tzerk <XnIASwkTCBlGgudcCsml>
2017-05-08 14:32:59 +0200

    add rlum/drac:1.1

---
+++ drac/1.1/index.php
@@ -0,0 +1,15 @@
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
+<script type='text/javascript' src='./drac.js' ></script>
+<link rel='stylesheet' media='screen' href='./drac.css' />
+</head>
+
+<body>
+<?php
+require_once("main/drac-init.php");
+require_once("main/drac-content.php");
+?>
+</body>
